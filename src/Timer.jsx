a913a5851cc899030a99cbb9122045cb2309079e
Saivaibhav K S (OSV) <saivaibhav.s.osv@fedex.com>
2024-11-20 22:16:06 +0530

    changes

---
+++ src/Timer.jsx
@@ -0,0 +1,11 @@
+const Timer = () => {
+  return (
+    <div className="container">
+      <input type="text" />
+      <input type="text" />
+      <input type="text" />
+    </div>
+  );
+};
+
+export default Timer;
