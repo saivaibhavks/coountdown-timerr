0ac693e33c4e132671ccfeead5bcc5958fc4cd24
Saivaibhav K S (OSV) <saivaibhav.s.osv@fedex.com>
2024-11-20 22:16:44 +0530

    changes

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -1,4 +1,12 @@
+import { useState } from "react";
+
 const Timer = () => {
+  const [time, setTime] = useState({
+    hour: 0,
+    min: 0,
+    sec: 0,
+  });
+
   return (
     <div className="container">
       <input type="text" />
